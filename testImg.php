b44803b0d8c7b9924df6c0f5190b3cae5f079f4d
icodeu <icodeyou@sina.com>
2014-08-31 10:23:44 +0800

    First Commit

---
+++ testImg.php
@@ -0,0 +1,26 @@
+<html>
+	<head>
+		<title>
+		</title>
+		<meta charset="UTF-8"/>
+	</head>
+	<body>
+		<div class="img_wd">
+			测试文字rtgdfgdfgdgdg
+		</div>
+		<style>
+		.img_wd {
+		
+			background: url(images/test.jpg) top center no-repeat;
+		
+			width: 100px;
+		
+			height: 100px;
+		
+			line-heiht: 100px;
+		
+			text-align: center;
+		
+		}</style>
+	</body>
+</html>
